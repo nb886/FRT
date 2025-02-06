633dd636fbbc559fd4316cb351fd5a3a7db61a5e
nb886 <72855509+nb886@users.noreply.github.com>
2025-02-06 23:49:47 +0800

    Add files via upload

---
+++ experiment-out_of_trans.py
@@ -0,0 +1,437 @@
+import gym
+import numpy as np
+import torch
+import wandb
+
+import argparse
+import pickle
+import random
+import sys
+import torch.nn.functional as F
+
+from d_mindformer.evaluation.evaluate_episodes_stop_early import (
+    evaluate_episode,
+    evaluate_episode_rtg,
+)
+
+# from d_mindformer.evaluation.evaluate_episodes import evaluate_episode, evaluate_episode_rtg
+# from d_mindformer.models.d_mindformer_change_more import DecisionMindformer
+# from d_mindformer.models.d_mindformer_change_more_rdg import DecisionMindformer
+
+# from d_mindformer.models.d_mindformer_change_getreward import DecisionMindformer
+# from d_mindformer.models.d_mindformer_out_trans_merge import DecisionMindformer
+from d_mindformer.models.d_mindformer_out_tran import DecisionMindformer
+# from d_mindformer.models.d_mindformer_out_trans_merge_dosth import DecisionMindformer
+
+
+# from d_mindformer.models.mlp_bc import MLPBCModel
+from d_mindformer.training.act_trainer import ActTrainer
+
+# from d_mindformer.training.seq_trainer_changemore_rtg import SequenceTrainer
+# from d_mindformer.training.seq_trainer_changemore import SequenceTrainer
+from d_mindformer.training.seq_trainer_getrewards import SequenceTrainer
+
+
+def discount_cumsum(x, gamma):
+    discount_cumsum = np.zeros_like(x)
+    discount_cumsum[-1] = x[-1]
+    for t in reversed(range(x.shape[0] - 1)):
+        discount_cumsum[t] = x[t] + gamma * discount_cumsum[t + 1]
+    return discount_cumsum
+
+
+def experiment(
+    exp_prefix,
+    variant,
+):
+    device = variant.get("device", "cuda")
+
+    log_to_wandb = variant.get("log_to_wandb", False)
+
+    env_name, dataset = variant["env"], variant["dataset"]
+    model_type = variant["model_type"]
+    group_name = f"{exp_prefix}-{env_name}-{dataset}"
+    exp_prefix = f"{group_name}-{random.randint(int(1e5), int(1e6) - 1)}"
+
+    if env_name == "hopper":
+        env = gym.make("Hopper-v3")
+        max_ep_len = 1000
+        env_targets = [3600, 1800]  # evaluation conditioning targets
+        scale = 1000.0  # normalization for rewards/returns
+    elif env_name == "halfcheetah":
+        env = gym.make("HalfCheetah-v3")
+        max_ep_len = 1000
+        env_targets = [12000, 6000]
+        scale = 1000.0
+    elif env_name == "walker2d":
+        env = gym.make("Walker2d-v3")
+        max_ep_len = 1000
+        env_targets = [5000, 2500]
+        scale = 1000.0
+    elif env_name == "reacher2d":
+        from d_mindformer.envs.reacher_2d import Reacher2dEnv
+
+        env = Reacher2dEnv()
+        max_ep_len = 100
+        env_targets = [76, 40]
+        scale = 10.0
+    else:
+        raise NotImplementedError
+    #
+    # if model_type == 'bc':
+    #     env_targets = env_targets[:1]  # since BC ignores target, no need for different evaluations
+
+    state_dim = env.observation_space.shape[0]
+    act_dim = env.action_space.shape[0]
+
+    # load dataset  输入数据
+    dataset_path = f"gym-eva/data/{env_name}-{dataset}-v2.pkl"
+    with open(dataset_path, "rb") as f:
+        trajectories = pickle.load(
+            f
+        )  # 一个 trajectory 就是一局行动直到 terminate = true
+
+    # save all path information into separate lists
+    mode = variant.get("mode", "normal")
+    states, traj_lens, returns = [], [], []
+    for path in trajectories:
+        if mode == "delayed":  # delayed: all rewards moved to end of trajectory
+            path["rewards"][-1] = path["rewards"].sum()
+            path["rewards"][:-1] = 0.0
+        states.append(path["observations"])  # state = observation
+        traj_lens.append(len(path["observations"]))  # traj_lens = observation 的长度
+        returns.append(path["rewards"].sum())  # return 是reward的总和
+    traj_lens, returns = np.array(traj_lens), np.array(returns)
+    # tensor = array 外面再加个框
+    # used for input normalization
+    states = np.concatenate(states, axis=0)  # state的总量
+    state_mean, state_std = (
+        np.mean(states, axis=0),
+        np.std(states, axis=0) + 1e-6,
+    )  # state的平均值和 std-标准差
+
+    num_timesteps = sum(traj_lens)
+
+    print("=" * 50)
+    print(f"Starting new experiment: {env_name} {dataset}")
+    print(f"{len(traj_lens)} trajectories, {num_timesteps} timesteps found")
+    print(f"Average return: {np.mean(returns):.2f}, std: {np.std(returns):.2f}")
+    print(f"Max return: {np.max(returns):.2f}, min: {np.min(returns):.2f}")
+    print("=" * 50)
+
+    K = variant["K"]
+    batch_size = variant["batch_size"]
+    num_eval_episodes = variant["num_eval_episodes"]
+    pct_traj = variant.get("pct_traj", 1.0)
+
+    # only train on top pct_traj trajectories (for %BC experiment)
+    num_timesteps = max(int(pct_traj * num_timesteps), 1)
+    sorted_inds = np.argsort(returns)  # lowest to highest
+    num_trajectories = 1
+    timesteps = traj_lens[sorted_inds[-1]]
+    ind = len(trajectories) - 2
+    while ind >= 0 and timesteps + traj_lens[sorted_inds[ind]] <= num_timesteps:
+        timesteps += traj_lens[sorted_inds[ind]]
+        num_trajectories += 1
+        ind -= 1
+    sorted_inds = sorted_inds[-num_trajectories:]
+
+    # used to reweight sampling so we sample according to timesteps instead of trajectories
+    p_sample = traj_lens[sorted_inds] / sum(traj_lens[sorted_inds])
+
+    def get_batch(
+        batch_size=256, max_len=K
+    ):  # get_batch函数通常用于从环境（Environment）或数据存储器（如 replay buffer）中获取一批（batch）状态、动作、奖励等。这些构成了一个经验（Experience）的序列，可以用于训练神经网络或策略
+        batch_inds = (
+            np.random.choice(  #  随机获得64组，根据timestep，timestep越长概率越大
+                np.arange(num_trajectories),
+                size=batch_size,
+                replace=True,
+                p=p_sample,  # reweights so we sample according to timesteps
+            )
+        )
+
+        s, a, r, d, rtg, timesteps, mask = [], [], [], [], [], [], []
+        for i in range(batch_size):
+            traj = trajectories[int(sorted_inds[batch_inds[i]])]  # 获取一个trajectory
+            si = random.randint(
+                0, traj["rewards"].shape[0] - 1
+            )  # 随机数（0~reward的长度-1）
+
+            # get sequences from dataset
+            s.append(
+                traj["observations"][si : si + max_len].reshape(1, -1, state_dim)
+            )  # 取index在si到si+20之间的state  # 20就是context，也就是NLP输出词语时考虑的语境范围
+            a.append(traj["actions"][si : si + max_len].reshape(1, -1, act_dim))
+            r.append(traj["rewards"][si : si + max_len].reshape(1, -1, 1))
+            if "terminals" in traj:
+                d.append(traj["terminals"][si : si + max_len].reshape(1, -1))
+            else:
+                d.append(traj["dones"][si : si + max_len].reshape(1, -1))
+            timesteps.append(np.arange(si, si + s[-1].shape[1]).reshape(1, -1))
+            timesteps[-1][timesteps[-1] >= max_ep_len] = (
+                max_ep_len - 1
+            )  # padding cutoff
+            rtg.append(
+                discount_cumsum(traj["rewards"][si:], gamma=1.0)[
+                    : s[-1].shape[1] + 1
+                ].reshape(1, -1, 1)
+            )  # returntogo 这个点到最后一个点(当前episode)的reward总和，这里的最后一个点不是si+20
+            if rtg[-1].shape[1] <= s[-1].shape[1]:
+                rtg[-1] = np.concatenate([rtg[-1], np.zeros((1, 1, 1))], axis=1)
+
+            # padding and state + reward normalization   由于si在（0，max_length-1）,si+20可能超过max_length，因此要在前面padding
+            tlen = s[-1].shape[1]
+            s[-1] = np.concatenate(
+                [np.zeros((1, max_len - tlen, state_dim)), s[-1]], axis=1
+            )
+            s[-1] = (s[-1] - state_mean) / state_std
+            a[-1] = np.concatenate(
+                [np.ones((1, max_len - tlen, act_dim)) * -10.0, a[-1]], axis=1
+            )
+            r[-1] = np.concatenate([np.zeros((1, max_len - tlen, 1)), r[-1]], axis=1)
+            d[-1] = np.concatenate([np.ones((1, max_len - tlen)) * 2, d[-1]], axis=1)
+            rtg[-1] = (
+                np.concatenate([np.zeros((1, max_len - tlen, 1)), rtg[-1]], axis=1)
+                / scale
+            )
+            timesteps[-1] = np.concatenate(
+                [np.zeros((1, max_len - tlen)), timesteps[-1]], axis=1
+            )
+            mask.append(
+                np.concatenate(
+                    [np.zeros((1, max_len - tlen)), np.ones((1, tlen))], axis=1
+                )
+            )
+
+        s = torch.from_numpy(np.concatenate(s, axis=0)).to(
+            dtype=torch.float32, device=device
+        )
+        a = torch.from_numpy(np.concatenate(a, axis=0)).to(
+            dtype=torch.float32, device=device
+        )
+        r = torch.from_numpy(np.concatenate(r, axis=0)).to(
+            dtype=torch.float32, device=device
+        )
+        d = torch.from_numpy(np.concatenate(d, axis=0)).to(
+            dtype=torch.long, device=device
+        )
+        rtg = torch.from_numpy(np.concatenate(rtg, axis=0)).to(
+            dtype=torch.float32, device=device
+        )
+        timesteps = torch.from_numpy(np.concatenate(timesteps, axis=0)).to(
+            dtype=torch.long, device=device
+        )
+        mask = torch.from_numpy(np.concatenate(mask, axis=0)).to(device=device)
+        # print(r.dtype)
+        # print(r.shape)
+        return s, a, r, d, rtg, timesteps, mask
+
+    # def eval_episodes(target_rew):
+    #     def fn(model):
+    #         returns, lengths = [], []
+    #         for _ in range(num_eval_episodes):
+    #             with torch.no_grad():
+    #                 if model_type == 'dt':
+    #                     ret, length = evaluate_episode_rtg(
+    #                         env,
+    #                         state_dim,
+    #                         act_dim,
+    #                         model,
+    #                         max_ep_len=max_ep_len,
+    #                         scale=scale,
+    #                         target_return=target_rew/scale,
+    #                         mode=mode,
+    #                         state_mean=state_mean,
+    #                         state_std=state_std,
+    #                         device=device,
+    #                     )
+    #                 else:
+    #                     ret, length = evaluate_episode(
+    #                         env,
+    #                         state_dim,
+    #                         act_dim,
+    #                         model,
+    #                         max_ep_len=max_ep_len,
+    #                         target_return=target_rew/scale,
+    #                         mode=mode,
+    #                         state_mean=state_mean,
+    #                         state_std=state_std,
+    #                         device=device,
+    #                     )
+    #             returns.append(ret)
+    #             lengths.append(length)
+    #         return {
+    #             f'target_{target_rew}_return_mean': np.mean(returns),
+    #             f'target_{target_rew}_return_std': np.std(returns),
+    #             f'target_{target_rew}_length_mean': np.mean(lengths),
+    #             f'target_{target_rew}_length_std': np.std(lengths),
+    #         }
+    #     return fn
+    def eval_episodes(target_rew):
+        def fn(model, stop):
+            returns, lengths = [], []
+            for _ in range(num_eval_episodes):
+                with torch.no_grad():
+                    if model_type == "dt":
+                        ret, length = evaluate_episode_rtg(
+                            env,
+                            state_dim,
+                            act_dim,
+                            model,
+                            max_ep_len=max_ep_len,
+                            scale=scale,
+                            target_return=target_rew / scale,
+                            mode=mode,
+                            state_mean=state_mean,
+                            state_std=state_std,
+                            device=device,
+                        )
+                    else:
+                        ret, length = evaluate_episode(
+                            env,
+                            state_dim,
+                            act_dim,
+                            model,
+                            max_ep_len=max_ep_len,
+                            target_return=target_rew / scale,
+                            mode=mode,
+                            state_mean=state_mean,
+                            state_std=state_std,
+                            device=device,
+                        )
+                returns.append(ret)
+                lengths.append(length)
+            if np.mean(returns) >= 10000 or stop == 1:
+                stop = 1
+            else:
+                stop = 0
+            return {
+                f"target_{target_rew}_return_mean": np.mean(returns),
+                f"target_{target_rew}_return_std": np.std(returns),
+                f"target_{target_rew}_length_mean": np.mean(lengths),
+                f"target_{target_rew}_length_std": np.std(lengths),
+            }, stop
+
+        return fn
+
+    if model_type == "dt":
+        model = DecisionMindformer(  # 建立DecisionTransformer
+            state_dim=state_dim,
+            act_dim=act_dim,
+            max_length=K,
+            max_ep_len=max_ep_len,
+            hidden_size=variant["embed_dim"],
+            n_layer=variant["n_layer"],
+            n_head=variant["n_head"],
+            n_inner=4 * variant["embed_dim"],
+            activation_function=variant["activation_function"],
+            n_positions=1024,
+            resid_pdrop=variant["dropout"],
+            attn_pdrop=variant["dropout"],
+        )
+    # elif model_type == 'bc':
+    #     model = MLPBCModel(
+    #         state_dim=state_dim,
+    #         act_dim=act_dim,
+    #         max_length=K,
+    #         hidden_size=variant['embed_dim'],
+    #         n_layer=variant['n_layer'],
+    #     )
+    else:
+        raise NotImplementedError
+
+    model = model.to(device=device)
+
+    warmup_steps = variant["warmup_steps"]
+    optimizer = torch.optim.AdamW(  # 建立optimizer
+        model.parameters(),
+        lr=variant["learning_rate"],
+        weight_decay=variant["weight_decay"],
+    )
+    scheduler = torch.optim.lr_scheduler.LambdaLR(  # 建立scheduler   改变learning rate
+        optimizer, lambda steps: min((steps + 1) / warmup_steps, 1)
+    )
+
+    if model_type == "dt":
+        trainer = SequenceTrainer(  # SequenceTrainer只比较actions 把model和 optiminzer scheduler 打包
+            model=model,
+            optimizer=optimizer,
+            batch_size=batch_size,
+            get_batch=get_batch,
+            scheduler=scheduler,
+            # loss_fn=lambda s_hat, a_hat, r_hat, s, a, r: torch.mean((a_hat - a)**2 + (r_hat - r)**2),
+            # loss_fn=lambda s_hat, a_hat, r_hat, s, a, r: 1 * torch.mean((a_hat - a) ** 2) + 0.05 * torch.mean(
+            #     (r_hat - r) ** 2),
+            loss_fn=lambda reward_hat, a_hat, r_hat, reward, a, r: 1
+            * F.smooth_l1_loss(a_hat, a, reduction="mean")
+            + 0.05 * F.smooth_l1_loss(r_hat, r, reduction="mean")
+            + 0.05 * F.smooth_l1_loss(reward_hat, reward, reduction="mean"),
+            eval_fns=[eval_episodes(tar) for tar in env_targets],
+        )
+    # elif model_type == 'bc':
+    #     trainer = ActTrainer(
+    #         model=model,
+    #         optimizer=optimizer,
+    #         batch_size=batch_size,
+    #         get_batch=get_batch,
+    #         scheduler=scheduler,
+    #         loss_fn=lambda s_hat, a_hat, r_hat, s, a, r: torch.mean((a_hat - a)**2),
+    #         eval_fns=[eval_episodes(tar) for tar in env_targets],
+    #     )
+
+    if log_to_wandb:
+        api_key = (
+            "6a4a014d20101c4d1dd5e3ae5566c72755be2221"  # 替换为你的实际WandB API密钥
+        )
+        wandb.login(key=api_key)
+        wandb.init(
+            name=exp_prefix,
+            group=group_name,
+            # project="Inference-transformer-less-step",
+            # project="Inference-transformer-5000-step",
+            project="Inference-transformer-2000-step-walker2d",
+            config=variant,
+        )
+        # wandb.watch(model)  # wandb has some bug
+
+    for iter in range(variant["max_iters"]):
+        outputs = trainer.train_iteration(
+            num_steps=variant["num_steps_per_iter"], iter_num=iter + 1, print_logs=True
+        )  # 将SequenceTrainer进行train_iteration,减小loss
+        if log_to_wandb:
+            wandb.log(outputs)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", type=str, default="walker2d")
+    parser.add_argument(
+        "--dataset", type=str, default="medium"
+    )  # medium, medium-replay, medium-expert, expert
+    parser.add_argument(
+        "--mode", type=str, default="normal"
+    )  # normal for standard setting, delayed for sparse
+    parser.add_argument("--K", type=int, default=20)
+    parser.add_argument("--pct_traj", type=float, default=1.0)
+    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument(
+        "--model_type", type=str, default="dt"
+    )  # dt for decision transformer, bc for behavior cloning
+    parser.add_argument("--embed_dim", type=int, default=128)
+    parser.add_argument("--n_layer", type=int, default=3)
+    parser.add_argument("--n_head", type=int, default=1)
+    parser.add_argument("--activation_function", type=str, default="relu")
+    parser.add_argument("--dropout", type=float, default=0.1)
+    parser.add_argument("--learning_rate", "-lr", type=float, default=1e-4)
+    parser.add_argument("--weight_decay", "-wd", type=float, default=1e-4)
+    parser.add_argument("--warmup_steps", type=int, default=10000)
+    parser.add_argument("--num_eval_episodes", type=int, default=100)
+    parser.add_argument("--max_iters", type=int, default=100)
+    parser.add_argument("--num_steps_per_iter", type=int, default=2000)
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--log_to_wandb", "-w", type=bool, default=True)
+
+    args = parser.parse_args()
+
+    experiment("ACT-out-trans", variant=vars(args))
